ab30e1030aacf92d315effa1950f2c941f71d5ae
Knowledge-JO <megamindtheincredible@gmail.com>
2023-11-15 11:53:09 +0100

    added height to form-2, 3, 4

--- src/components/forms/Form3.jsx
+++ src/components/forms/Form3.jsx
@@ -1,6 +1,6 @@
 const Form3 = () => {
   return (
-    <div className="text-white px-10 md:px-[100px]  mt-[30px]">
+    <div className="text-white px-10 md:px-[100px] h-[250px] mt-[30px]">
       <h1 className="font-bold text-[30px] text-center mb-10">ANTI-BOT</h1>
     </div>
   );
--- src/components/forms/Form4.jsx
+++ src/components/forms/Form4.jsx
@@ -1,6 +1,6 @@
 const Form4 = () => {
   return (
-    <div className="text-white px-10 md:px-[100px]  mt-[30px]">
+    <div className="text-white px-10 md:px-[100px] h-[250px] mt-[30px]">
       <h1 className="font-bold text-[30px] text-center mb-10">Taxes</h1>
     </div>
   );
