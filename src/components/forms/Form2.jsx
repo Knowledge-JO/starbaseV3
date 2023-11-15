9bd7c51ec591da9cfd82ed1f868fec09a486d698
Knowledge-JO <megamindtheincredible@gmail.com>
2023-11-15 11:58:38 +0100

    removed h from forms and placed in main

--- src/components/forms/Form2.jsx
+++ src/components/forms/Form2.jsx
@@ -1,6 +1,6 @@
 const Form2 = () => {
   return (
-    <div className="text-white px-10 md:px-[100px] h-[100vh] mt-[30px]">
+    <div className="text-white px-10 md:px-[100px] mt-[30px]">
       <h1 className="font-bold text-[30px] text-center mb-10">Distribution</h1>
     </div>
   );
