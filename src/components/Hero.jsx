02e11f08a81e29bc95044a9acd8402ba0b8814e8
Knowledge-JO <megamindtheincredible@gmail.com>
2023-11-16 15:11:32 +0100

    typwriting animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,38 @@
+import { useEffect, useRef } from "react";
+
 const Hero = () => {
+  const spanElement = useRef(null);
+
+  useEffect(() => {
+    const current = spanElement.current;
+    const textLoad = () => {
+      setInterval(() => {
+        current.textContent = "Ethereum Blockchain";
+      }, 4000);
+      setInterval(() => {
+        current.textContent = "Binance Smart Chain";
+      }, 8000);
+      setInterval(() => {
+        current.textContent = "Arbitrum Blockchain";
+      }, 12000);
+    };
+    // setInterval(textLoad, 12000);
+    textLoad();
+  }, [spanElement.current]);
   return (
-    <div className="text-white text-center px-10">
-      <h1 className="text-[25px] mt-[200px] font-bold">Token Deployer</h1>
-      <p className="text-sm">Create and deploy your own ERC20 token</p>
+    <div className="text-white  px-10">
+      <div className="mt-[200px] flex justify-center text-center">
+        <h1 className=" mr-2 font-bold text-[20px] lg:text-[25px]">
+          Deploy token on{" "}
+          <span
+            ref={spanElement}
+            className="block overflow-hidden text-[20px] lg:text-[25px] text-red-500 font-bold relative my-[5px] mx-0  after:absolute after:left-0 after:bottom-0 after:h-[100%] after:w-[100%] after:bg-black after:border-l-2 after:border-red-500 after:animate-typing_anim "
+          ></span>
+        </h1>
+      </div>
+      <p className="text-center mt-3">
+        Creating your token for your dream project with just few click
+      </p>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slide: {
+          "100%": {
+            top: "-360px",
+          },
+        },
+
+        typing: {
+          "40%, 60%": {
+            left: "100%",
+          },
+          "100%": {
+            left: "0%",
+          },
+        },
+        blink: {
+          "50%": {
+            opacity: "0",
+          },
+        },
+      },
+
+      animation: {
+        slide_anim: "slide 12s steps(12) infinite",
+        typing_anim: "typing 4s infinite",
+      },
+    },
   },
   plugins: [],
 };
