aca69d4feb9bb8e1a48c128694f54e51d87b7877
Knowledge-JO <megamindtheincredible@gmail.com>
2023-12-18 23:05:05 +0100

    updated Form4

--- src/components/forms/Form4.jsx
+++ src/components/forms/Form4.jsx
@@ -19,11 +19,26 @@ const Form4 = ({state, dispatch}) => {
             <InputFeature feature={"Sell Tax"} label={"Tax taken for token sell"} state={state.sellTaxSlide}>
               <InputSlider state={state.sellTaxSlide} dispatch={dispatch} type="sellTaxSlide"/> 
             </InputFeature>
+            <InputAddress state={state.sellTaxSlide || state.buyTaxSlide}/>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+
+const InputAddress = ({state}) => {
+  return (
+    <div className={`${state ? "h-[120px] border-2 md:border-2 border-white/40": "h-0 border-none"}  rounded-lg w-full min-[600px]:w-[400px] px-4 min-[400px]:px-8 py-3 text-[12px] md:text-base transition-height duration-300`}>
+      <div className={`${state ? "block": "hidden"}`}>
+        <div className="flex justify-between items-center mb-2">
+          <p>Tax Payout Address</p>
+        </div>
+        <input className={`bg-[#262626] h-[40px] w-[100%] rounded-md px-3 ring-1 ring-gray-700 focus:outline focus:outline-red-500`} type="text" id="max-token"/>
+      </div>
+    </div>
+  )
+}
 
 export default Form4;
