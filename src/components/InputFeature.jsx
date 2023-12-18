72a89a65a77c85dbf6ef76a604a038b7a0ccbfa9
Knowledge-JO <megamindtheincredible@gmail.com>
2023-12-18 22:19:49 +0100

    updated Form3

---
+++ src/components/InputFeature.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line react/prop-types
+const InputFeature = ({feature, label,  state, children}) => {
+  return (
+    <div className="border-2 border-white/40 md:border-2  mb-3 rounded-lg w-full min-[600px]:w-[400px] px-4 min-[400px]:px-8 py-3 text-[12px] md:text-base">
+      <div className="flex justify-between items-center">
+        <p>{feature}</p>
+        {children}
+      </div>
+      <div className={`${state ? "h-20" : " h-0"} transition-height duration-300 mt-3 mb-3`}>
+        <label htmlFor="max-token" className={`${state ? "block": "hidden"} text-sm ml-3 mb-1`}>{label}</label>
+        <div className={`${state ? "block": "hidden"} relative`}>
+          <input className={`bg-[#262626] h-[40px] w-[100%] rounded-md px-7 ring-1 ring-gray-700 focus:outline focus:outline-red-500`} type="number" max={1} id="max-token"/>
+          <span className="absolute top-0 h-full flex items-center px-3">%</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InputFeature
--- src/components/forms/Form3.jsx
+++ src/components/forms/Form3.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 // import { useState } from "react";
 import InputSlider from "../InputSlider";
-
+import InputFeature from "../InputFeature";
 
 const Form3 = ({state, dispatch}) => {
   return (
@@ -14,12 +14,12 @@ const Form3 = ({state, dispatch}) => {
      <div className="flex justify-center items-center">
         <div className="w-full flex justify-center items-center md:w-fit bg-[#262626] md:px-20 py-8 rounded-xl mt-5 ">
           <div className="w-full min-[600px]:w-fit px-2">
-            <AnitBotFeature feature={"Limit per wallet"} label={"Max tokens per wallet"} state={state.sliderState1}> 
+            <InputFeature feature={"Limit per wallet"} label={"Max tokens per wallet"} state={state.sliderState1}> 
               <InputSlider state={state.sliderState1} dispatch={dispatch} type="slider1"/> 
-            </AnitBotFeature>
-            <AnitBotFeature feature={"Max per transaction"} label={"Max tokens per trade"} state={state.sliderState2}>
+            </InputFeature>
+            <InputFeature feature={"Max per transaction"} label={"Max tokens per trade"} state={state.sliderState2}>
               <InputSlider state={state.sliderState2} dispatch={dispatch} type="slider2"/> 
-            </AnitBotFeature>
+            </InputFeature>
             <SniperAutoBurnFeature feature={"Sniper auto-burn"} state={state.sliderState3}>
               <InputSlider state={state.sliderState3} dispatch={dispatch} type="slider3"/>
             </SniperAutoBurnFeature>
@@ -50,23 +50,6 @@ const SniperAutoBurnFeature = ({feature, state, children}) => {
 }
 
 
-// eslint-disable-next-line react/prop-types
-const AnitBotFeature = ({feature, label,  state, children}) => {
-  return (
-    <div className="border-2 border-white/40 md:border-2  mb-3 rounded-lg w-full min-[600px]:w-[400px] px-4 min-[400px]:px-8 py-3 text-[12px] md:text-base">
-      <div className="flex justify-between items-center">
-        <p>{feature}</p>
-        {children}
-      </div>
-      <div className={`${state ? "h-20" : " h-0"} transition-height duration-300 mt-3 mb-3`}>
-        <label htmlFor="max-token" className={`${state ? "block": "hidden"} text-sm ml-3 mb-1`}>{label}</label>
-        <div className={`${state ? "block": "hidden"} relative`}>
-          <input className={`bg-[#262626] h-[40px] w-[100%] rounded-md px-7 ring-1 ring-gray-700 focus:outline focus:outline-red-500`} type="number" max={1} id="max-token"/>
-          <span className="absolute top-0 h-full flex items-center px-3">%</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+
 
 export default Form3;
