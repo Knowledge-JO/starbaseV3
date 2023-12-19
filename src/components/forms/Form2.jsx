a0d064eda499effd804891ba21396972763914f2
Knowledge-JO <megamindtheincredible@gmail.com>
2023-12-19 21:45:09 +0100

    update

--- src/components/forms/Form2.jsx
+++ src/components/forms/Form2.jsx
@@ -2,6 +2,9 @@ const Form2 = () => {
   return (
     <div className="text-white px-10 md:px-[100px] mt-[30px]">
       <h1 className="font-bold text-[30px] text-center mb-10">Distribution</h1>
+      <div className="">
+        
+      </div>
     </div>
   );
 };
